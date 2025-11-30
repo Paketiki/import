f9a11d40d35549710877d990bd64b59389052702
Rokima <55d55r77@gmail.com>
2025-11-30 21:55:13 +0300

    add model users

---
+++ app/models/users.py
@@ -0,0 +1,14 @@
+from sqlalchemy import String, Float, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from app.database.database import Base
+
+
+class UserModel(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(300), nullable=False)
+
+    role_id: Mapped[int] = mapped_column(ForeignKey("roles.id"), nullable=False)
+    role: Mapped["RoleModel"] = relationship(back_populates="users")
