cf999368360c629c2420f1633e85dbb26bad71de
Rokima <55d55r77@gmail.com>
2025-11-30 21:59:38 +0300

    update model users

--- app/models/users.py
+++ app/models/users.py
@@ -1,6 +1,11 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String, Float, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.database.database import Base
+
+if TYPE_CHECKING:
+    from app.models.roles import RoleModel
 
 
 class UserModel(Base):
