5938fa1ee82768646b7c253244e504e5218f7756
Podlepich_Artem <112714253+Paketiki@users.noreply.github.com>
2025-12-12 01:53:55 +0300

    Fix: Add relationships to MoviePick model

--- app/models/movie_picks.py
+++ app/models/movie_picks.py
@@ -1,6 +1,6 @@
-# app/models/movie_picks.py
 from sqlalchemy import Column, Integer, DateTime, ForeignKey
 from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
 from app.database.base import Base
 
 class MoviePick(Base):
@@ -9,3 +9,7 @@ class MoviePick(Base):
     movie_id = Column(Integer, ForeignKey("movies.id"), primary_key=True)
     pick_id = Column(Integer, ForeignKey("picks.id"), primary_key=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+    
+    # Отношения
+    movie = relationship("Movie", back_populates="picks")
+    pick = relationship("Pick", back_populates="movies")
